1da6d7502921e483bde9a18b8e932dd1274cb529
Zijia Zhai <zijiazhai@gmail.com>
2022-07-29 17:26:45 -0500

    init

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Zijia Zhai on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
