b8f3130bd94607e0152e49050fbb75fb08361ba0
Zijia Zhai <zijiazhai@gmail.com>
2022-08-12 13:18:21 -0500

    Todo List - last

--- ToDoList/ToDoList/ViewModels/ListViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ListViewModel.swift
@@ -10,14 +10,18 @@ import Foundation
 class ListViewModel: ObservableObject {
     
     @Published var items: [ItemModel] = []
+    let itemsKey = "itemsKey"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [ItemModel(title: "1st title", isCompleted: true), ItemModel(title: "2ed title", isCompleted: false), ItemModel(title: "3rd title", isCompleted: false)]
-        items.append(contentsOf: newItems)
+//        let newItems = [ItemModel(title: "1st title", isCompleted: true), ItemModel(title: "2ed title", isCompleted: false), ItemModel(title: "3rd title", isCompleted: false)]
+//        items.append(contentsOf: newItems)
+        guard let data = UserDefaults.standard.data(forKey: itemsKey),
+              let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data) else { return }
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -36,6 +40,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodeData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodeData, forKey: itemsKey)
         }
     }
 }
--- ToDoList/ToDoList/Views/ListView.swift
+++ ToDoList/ToDoList/Views/ListView.swift
@@ -12,19 +12,25 @@ struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     
     var body: some View {
-        List {
-            ForEach(listViewModel.items) { item in
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            if listViewModel.items.isEmpty {
+                Text("123")
+            } else {
+                List {
+                    ForEach(listViewModel.items) { item in
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(.plain)
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
-        .listStyle(.plain)
         .navigationTitle("Todo List 📝")
         .navigationBarItems(leading: EditButton(),
                             trailing: NavigationLink("Add", destination: AddView()))
