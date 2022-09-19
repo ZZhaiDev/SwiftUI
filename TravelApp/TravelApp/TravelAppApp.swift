07b2337ba4c3fe067c8288d5e08c0d4c362bd775
Zijia Zhai <zijiazhai@gmail.com>
2022-09-18 19:36:49 -0500

    Travel App: Discover Page v1

---
+++ TravelApp/TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Zijia Zhai on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
